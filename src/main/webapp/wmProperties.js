e850f773f0842c4386e04d598d4b0856e1c7aba1
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-22 23:16:40 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_WLmYVqgSkk",
+  "homePage" : "Main",
+  "name" : "Auto_WLmYVqgSkk",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
